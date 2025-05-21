8609e4bc6e3b66c836e69a4c41ab4850c3c2c2fe
Michael Cimolini <mcimolini@nait.ca>
2025-05-20 19:54:32 -0600

    Implemented Fields, Attributes and Default Contsructor based on first Fact test.

--- OOPsReview/OOPsReview/Person.cs
+++ OOPsReview/OOPsReview/Person.cs
@@ -8,5 +8,43 @@ namespace OOPsReview
 {
     public class Person
     {
+        private string _FirstName;
+        private string _LastName;
+
+        #region Attributes
+
+        public ResidentAddress Address { get; set; }
+        public List<Employment> EmploymentPositions { get; set; }
+
+        public string FirstName
+        { 
+            get { return _FirstName; }
+            set { _FirstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _LastName; }
+            set { _LastName = value; }
+        }
+
+        public string FullName
+        {
+            get { return $"{FirstName} {LastName}"; } 
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public Person()
+        {
+            FirstName = "Unknown";
+            LastName = "Unknown";
+
+            EmploymentPositions = new List<Employment>();
+        }
+
+        #endregion
     }
 }
