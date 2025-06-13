ae91135d4251b830002b11444d71d2b72bb55e9b
Michael Cimolini <mcimolini@nait.ca>
2025-06-12 20:28:07 -0600

    Set up IJSRuntime and imlemented a confirmation dialog in my clear method.

--- BlazorWebApp/BlazorWebApp/Components/Pages/InClass/DataEntry.razor.cs
+++ BlazorWebApp/BlazorWebApp/Components/Pages/InClass/DataEntry.razor.cs
@@ -1,5 +1,6 @@
 ﻿using OOPsReview;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace BlazorWebApp.Components.Pages.InClass
 {
@@ -20,6 +21,10 @@ namespace BlazorWebApp.Components.Pages.InClass
         //Gives access to our Web Host (Browser) to determine where our application is running
         [Inject]
         IWebHostEnvironment WebHostEnvironment { get; set; }
+
+        //Add the ability to use JS functions
+        [Inject]
+        IJSRuntime JSRuntime { get; set; }
 
         /// <summary>
         /// Checks all of our fields for errors.
@@ -92,15 +97,21 @@ namespace BlazorWebApp.Components.Pages.InClass
         /// <summary>
         /// Resets all of our fields to default values.
         /// </summary>
-        private void Clear()
+        private async Task Clear()
         {
-            FeedbackMsg = String.Empty;
-            ErrorMsgs.Clear();
+            string message = "Clearing will lose all unsaved data. Are you sure you wish to continue?";
 
-            EmploymentTitle = String.Empty;
-            EmploymentYears = 0.0;
-            EmploymentStart = DateTime.Today;
-            EmploymentLevel = SupervisoryLevel.Entry;
+            //Creates a JS popup in my browser that has a confirmation window with the provided message
+            if (await JSRuntime.InvokeAsync<bool>("confirm", message))
+            {
+                FeedbackMsg = String.Empty;
+                ErrorMsgs.Clear();
+
+                EmploymentTitle = String.Empty;
+                EmploymentYears = 0.0;
+                EmploymentStart = DateTime.Today;
+                EmploymentLevel = SupervisoryLevel.Entry;
+            }
         }
 
         /// <summary>
