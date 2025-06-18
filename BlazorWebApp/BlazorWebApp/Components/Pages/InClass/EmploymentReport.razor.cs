07939522aafc022977be780ddcc1eb47b8bc7e32
Michael Cimolini <mcimolini@nait.ca>
2025-06-17 20:02:04 -0600

    Added Employment list and set up all default values.

--- BlazorWebApp/BlazorWebApp/Components/Pages/InClass/EmploymentReport.razor.cs
+++ BlazorWebApp/BlazorWebApp/Components/Pages/InClass/EmploymentReport.razor.cs
@@ -1,7 +1,12 @@
-﻿namespace BlazorWebApp.Components.Pages.InClass
+﻿using OOPsReview;
+
+namespace BlazorWebApp.Components.Pages.InClass
 {
     public partial class EmploymentReport
     {
+        string FeedbackMsg = String.Empty;
+        List<string> ErrorMsgs = new(); //Creats a new version of the variable type on the left. Same as new List<string>();
 
+        List<Employment> Employments = new();
     }
 }
