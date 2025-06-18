d63b7bfb9ee300b0f3ff20a28e582a66f64ae84c
Michael Cimolini <mcimolini@nait.ca>
2025-06-17 20:17:20 -0600

    Copied GetInnerException from other file.

--- BlazorWebApp/BlazorWebApp/Components/Pages/InClass/EmploymentReport.razor.cs
+++ BlazorWebApp/BlazorWebApp/Components/Pages/InClass/EmploymentReport.razor.cs
@@ -8,5 +8,22 @@ namespace BlazorWebApp.Components.Pages.InClass
         List<string> ErrorMsgs = new(); //Creats a new version of the variable type on the left. Same as new List<string>();
 
         List<Employment> Employments = new();
+
+
+
+        /// <summary>
+        /// Returns the deepest nested exception.
+        /// </summary>
+        /// <param name="ex">The exception to check.</param>
+        /// <returns></returns>
+        private Exception GetInnerException(Exception ex)
+        {
+            while (ex.InnerException != null)
+            {
+                ex = ex.InnerException;
+            }
+
+            return ex;
+        }
     }
 }
