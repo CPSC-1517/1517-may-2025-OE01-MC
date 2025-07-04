2e61b580efd36221d93e93bd8aa52b0e4f62da7e
Michael Cimolini <mcimolini@nait.ca>
2025-07-03 21:20:55 -0600

    Set up code behind with placeholder variables/methods.

--- CSWebApp/WestWindApp/Components/Pages/Regions.razor.cs
+++ CSWebApp/WestWindApp/Components/Pages/Regions.razor.cs
@@ -1,9 +1,32 @@
-﻿namespace WestWindApp.Components.Pages
+﻿using WestWindSystem.Entities;
+
+namespace WestWindApp.Components.Pages
 {
     public partial class Regions
     {
         string FeedbackMessage = String.Empty;
         List<string> ErrorMsgs = new();
 
+        int TargetID = 0;
+        int SelectionID = 0;
+
+        Region _Region = null;
+
+        List<Region> RegionsList = new();
+
+        void GetByID()
+        {
+
+        }
+
+        void GetBySelect()
+        {
+
+        }
+
+        void ClearData()
+        {
+
+        }
     }
 }
