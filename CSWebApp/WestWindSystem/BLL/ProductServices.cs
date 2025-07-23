e60ae7853691414fb7cadf644723e7f687da00fe
Michael Cimolini <mcimolini@nait.ca>
2025-07-22 20:46:53 -0600

    Implemented Create

--- CSWebApp/WestWindSystem/BLL/ProductServices.cs
+++ CSWebApp/WestWindSystem/BLL/ProductServices.cs
@@ -38,7 +38,45 @@ namespace WestWindSystem.BLL
         /// <returns>The ID of the added product.</returns>
         public int AddProduct(Product product)
         {
+            //Validate we actually have data.
 
+            if (product == null)
+            {
+                throw new ArgumentNullException("Product Information Required!");
+            }
+
+            //Validate and business rules/logic
+            //BL is that no 2 products can have the same SupplierID, ProductName, and QuantityPerUnit
+
+            bool exists = false;
+
+            exists = _context.Products.Any( x => x.SupplierID == product.SupplierID &&
+                                                 x.ProductName == product.ProductName &&
+                                                 x.QuantityPerUnit == product.QuantityPerUnit);
+
+            //Throw an error if there's a duplicate
+            if(exists)
+            {
+                throw new ArgumentException("Product already exists!");
+            }
+
+            //Could test that values are in acceptable ranges here
+
+            //Assume our data is good at this point
+
+            //Step 1: Staging our Data
+            //Our data is only in local memory at this point
+            //Any final changes happen here ie. default values, data type changes, date formatting, etc
+
+            _context.Products.Add(product);
+
+            //Step 2: Commit
+            //This pushes our data to our DB
+            //Entity Annotation Validation will happen before it's passed to the database
+
+            _context.SaveChanges(); //This automatically sets our PKey ID
+
+            return product.ProductID;
         }
 
         #endregion
